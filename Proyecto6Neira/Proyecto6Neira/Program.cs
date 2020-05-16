7aa22b3d4755b8cb42d58c8840e26b877a0b1c66
Cristobal Neira <62522973+cristobalneira@users.noreply.github.com>
2020-05-16 18:06:15 -0400

    Creacion

---
+++ Proyecto6Neira/Proyecto6Neira/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto6Neira
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
