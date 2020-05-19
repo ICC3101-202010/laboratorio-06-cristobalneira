fd94ec48199c56e7a88d62eba7cb45f41aa1e9a4
Cristobal Neira <62522973+cristobalneira@users.noreply.github.com>
2020-05-19 14:32:53 -0400

    Last commit

--- Proyecto6Neira/Proyecto6Neira/Program.cs
+++ Proyecto6Neira/Proyecto6Neira/Program.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Proyecto6Neira
 {
@@ -12,29 +13,29 @@ namespace Proyecto6Neira
         public static void Main(string[] args)
         {
             Persona per1 = new Persona("Tomas", "Neira", 190712420, 1);
-            Persona per2 = new Persona("Rocio", "Roberira", 190712426, 1);
-            Persona ebq1 = new Persona("Rocio", "Roberira", 190712426, 2);
+            Persona per2 = new Persona("Rocio", "Roberira", 190352426, 1);
+            Persona ebq1 = new Persona("Michael", "Jackson", 133712426, 2);
             Bloque bloque1 = new Bloque(ebq1, "Bloque 1");
             bloque1.listadetrabajadores.Add(per1);
             bloque1.listadetrabajadores.Add(per2);
 
-            Persona per3 = new Persona("Consuelo", "Laso", 190715420, 1);
-            Persona per4 = new Persona("Rene", "Jeira", 190782420, 1);
-            Persona ebq2 = new Persona("Rene", "Jeira", 190782420, 2);
+            Persona per3 = new Persona("Consuelo", "Laso", 134655420, 1);
+            Persona per4 = new Persona("Rene", "Jeira", 102082420, 1);
+            Persona ebq2 = new Persona("John", "Cena", 19982420, 2);
             Bloque bloque2 = new Bloque(ebq2, "Bloque 2");
             bloque2.listadetrabajadores.Add(per3);
             bloque2.listadetrabajadores.Add(per4);
 
             Persona per5 = new Persona("Juan", "Neira", 190792420, 1);
             Persona per6 = new Persona("Camila", "Joebel", 190312420, 1);
-            Persona ebq3 = new Persona("Camila", "Joebel", 190312420, 2);
+            Persona ebq3 = new Persona("Lana", "Rhodes", 190312420, 2);
             Bloque bloque3 = new Bloque(ebq3, "Bloque 3");
             bloque3.listadetrabajadores.Add(per5);
             bloque3.listadetrabajadores.Add(per6);
 
-            Persona per7 = new Persona("Juan", "Neira", 190792420, 1);
-            Persona per8 = new Persona("Camila", "Joebel", 190312420,1);
-            Persona ebq4 = new Persona("Camila", "Joebel", 190312420, 2);
+            Persona per7 = new Persona("Pedro", "Mancilla", 190792420, 1);
+            Persona per8 = new Persona("Javier", "Flores", 190312420,1);
+            Persona ebq4 = new Persona("Roberto", "Firminio", 190312420, 2);
             Bloque bloque4 = new Bloque(ebq4, "Bloque 4");
             bloque4.listadetrabajadores.Add(per7);
             bloque4.listadetrabajadores.Add(per8);
@@ -45,7 +46,7 @@ namespace Proyecto6Neira
             seccion1.listadebloques.Add(bloque2);
 
             Persona esecc2 = new Persona("Fernando", "Perez", 256788887, 3);
-            Seccion seccion2 = new Seccion(esecc1, "Seccion2");
+            Seccion seccion2 = new Seccion(esecc2, "Seccion2");
             seccion2.listadebloques.Add(bloque3);
             seccion2.listadebloques.Add(bloque4);
 
@@ -65,55 +66,213 @@ namespace Proyecto6Neira
             Persona ceo = new Persona("Cristobal", "Neira", 200712420, 7);
             Empresa empresa = new Empresa(per1, "8inc", 006960272);
             empresa.listadivisiones.Add(div1);
-            
-            Console.WriteLine("Bienvenido a la visualizacion de empresas!");
-            Console.WriteLine("Quiere utilizar un archivo para cargar informacion de la empresa?");
-            Console.WriteLine("[1] Si\n[2] No\n[3] Ver todas las [0] Salir");
-            int p1 = Convert.ToInt32(Console.ReadLine());
-            //Salir falta poner el while nomas
-            if (p1 == 0)
-            {
 
-            }
-            //Poner info
-            else if (p1 == 2)
+            Empresa empresa2 = empresa;
+
+            Console.WriteLine("Bienvenido a la visualizacion de empresa!");
+            int a = 0;
+            while(a==0)
             {
-                Console.WriteLine("Ingrese nombre de la empresa:");
-                string name = Console.ReadLine();
-                Console.WriteLine("Ingrese rut de la empresa (solo 9 numeros):");
-                int rut = Convert.ToInt32(Console.ReadLine());
-                Empresa emp = new Empresa(ceo,name, rut);
-                FileStream fs = new FileStream("empresa.bit", FileMode.Create);
-                BinaryFormatter bf = new BinaryFormatter();
-                try
+                int o = 0;
+                while (o == 0)
                 {
-                    bf.Serialize(fs, emp);
+                    Console.WriteLine("Quiere hacer con la informacion de la empresa?");
+                    Console.WriteLine("[1] Cargar Informacion\n[2] No Cargar Informacion\n[0] Salir");
+                    try
+                    {
+                        o = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    }
+                    catch (System.FormatException e)
+                    {
+                        Console.WriteLine("Error:" + e.Message);
+                        Console.WriteLine("Ingrese valor entre 0-2!");
+                    }
                 }
-                catch (SerializationException e)
+                int p1 = o; 
+                if (p1 == 1)
                 {
-                    Console.WriteLine("Failed to serialize. Reason: " + e.Message);
-                    throw;
+                    try
+                    {
+                        IFormatter formatter = new BinaryFormatter();
+                        Stream file = new FileStream("empresa.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+                        Empresa empresas2 = (Empresa)formatter.Deserialize(file);
+                        file.Close();
+                        Console.WriteLine("Leyendo informacion empresa...");
+                        Thread.Sleep(2000);
+                        Console.WriteLine("Empresa: " + empresa2.nombre+ " Rut: "+empresa2.rut);
+                        Console.WriteLine("---------------------------------------------------------------");
+                        foreach (var item in empresa2.listadivisiones)
+                        {
+
+                            Console.WriteLine("Division: " + item.nombre_division+" Encargado: " + item.EncargadoD.nombre+ " " + item.EncargadoD.apellido);
+                        }
+                        Thread.Sleep(1000);
+                        Console.WriteLine("---------------------------------------------------------------");
+                        foreach (var item in empresa2.listadivisiones)
+                        {
+
+                            foreach (var item2 in item.listadeareas)
+                            {
+
+                                Console.WriteLine("Area: " + item2.nombre_area+" Encargado: " + item2.EncargadoA.nombre+ " " +item2.EncargadoA.apellido);
+                            }
+                        }
+                        Thread.Sleep(1000);
+                        Console.WriteLine("---------------------------------------------------------------");
+                        foreach (var item in empresa2.listadivisiones)
+                        {
+
+                            foreach (var item2 in item.listadeareas)
+                            {
+                                foreach (var item3 in item2.listadedep)
+                                {
+                                    Console.WriteLine("Departamento: " + item3.nombre_departamento+" Encargado: " + item3.EncargadoD.nombre+ " " +item3.EncargadoD.apellido );
+                                }
+                            }
+                        }
+                        Thread.Sleep(1000);
+                        Console.WriteLine("---------------------------------------------------------------");
+                        foreach (var item in empresa2.listadivisiones)
+                        {
+                            foreach (var item2 in item.listadeareas)
+                            {
+                                foreach (var item3 in item2.listadedep)
+                                {
+                                    foreach (var item4 in item3.listadesecciones)
+                                    {
+                                        Console.WriteLine("Seccion: " + item4.nombre_seccion+" Encargado: " + item4.EncargadoS.nombre+" "+item4.EncargadoS.apellido);
+                                    }
+                                }
+                            }
+                        }
+                        Thread.Sleep(1000);
+                        Console.WriteLine("---------------------------------------------------------------");
+                        foreach (var item in empresa2.listadivisiones)
+                        {
+                            foreach (var item2 in item.listadeareas)
+                            {
+                                foreach (var item3 in item2.listadedep)
+                                {
+                                    foreach (var item4 in item3.listadesecciones)
+                                    {
+                                        foreach (var item5 in item4.listadebloques)
+                                        {
+                                            Console.WriteLine("Bloque: " + item5.nombre_bloque+" Encargado: " + item5.EncargadoB.nombre +" "+ item5.EncargadoB.apellido);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        Thread.Sleep(1000);
+                        Console.WriteLine("---------------------------------------------------------------");
+                        foreach (var item in empresa2.listadivisiones)
+                        {
+                            foreach (var item2 in item.listadeareas)
+                            {
+                                foreach (var item3 in item2.listadedep)
+                                {
+                                    foreach (var item4 in item3.listadesecciones)
+                                    {
+                                        foreach (var item5 in item4.listadebloques)
+                                        {
+                                            Console.WriteLine(item5.nombre_bloque);
+                                            Console.WriteLine("Encargado: " + item5.EncargadoB);
+                                            foreach (var item6 in item5.listadetrabajadores)
+                                            {
+                                                Thread.Sleep(1000);
+                                                Console.WriteLine("Trabajador: " + item6.nombre+" "+item6.apellido + ", Rut: " + item6.rut);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (FileNotFoundException ez)
+                    {
+                        Console.WriteLine("Failed to serialize. Reason: " + ez.Message);
+                        Thread.Sleep(3000);
+                        Console.WriteLine("Ingrese estos datos para crear una empresa y serializarla:");
+                        Console.WriteLine("Ingrese nombre de la empresa:");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Ingrese rut de la empresa (solo 9 numeros):");
+                        int rut = 0;
+                        try
+                        {
+                            rut = Convert.ToInt32(Console.ReadLine());
+                        }
+                        catch (FormatException ep)
+                        {
+                            Console.WriteLine("Error:" + ep.Message);
+                            Console.WriteLine("Ingrese solo numeros!");
+                            Console.WriteLine("Ingrese rut de la empresa (solo numeros):");
+                        }
+                        rut = Convert.ToInt32(Console.ReadLine());
+                        empresa2.nombre = name;
+                        empresa2.rut = rut;
+                        Console.WriteLine("Empresa creada!");
+                        Thread.Sleep(3000);
+                        FileStream file = new FileStream("empresa.bin", FileMode.Create);
+                        IFormatter bf = new BinaryFormatter();
+                        bf.Serialize(file, empresa2);
+                        file.Close();
+                        Console.WriteLine("Empresa serializada!");
+                        Thread.Sleep(3000);
+
+                        Console.WriteLine("Empresa serializada!");
+                        Thread.Sleep(2000);
+                    }
+                    finally
+                    {
+                        Console.WriteLine("Datos cargados!");
+                        
+                    }
+                    
+
                 }
-                finally
+                else if (p1 == 2)
                 {
-                    fs.Close();
+                    Console.WriteLine("Ingrese estos datos para crear una empresa:");
+                    Console.WriteLine("Ingrese nombre de la empresa:");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Ingrese rut de la empresa (solo numeros):");
+                    int rut = 0;
+                    try
+                    {
+                        rut = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch(FormatException e)
+                    {
+                        Console.WriteLine("Error:" + e.Message);
+                        Console.WriteLine("Ingrese solo numeros!");
+                        Console.WriteLine("Ingrese rut de la empresa (solo numeros):");
+                    }
+                    rut = Convert.ToInt32(Console.ReadLine());
+                    empresa2.nombre = name;
+                    empresa2.rut = rut;
+                    Console.WriteLine("Empresa creada!");
+                    Thread.Sleep(3000);
+                    FileStream file = new FileStream("empresa.bin", FileMode.Create);
+                    IFormatter bf = new BinaryFormatter();
+                    bf.Serialize(file, empresa2);
+                    file.Close();
+                    Console.WriteLine("Empresa serializada!");
+                    Thread.Sleep(3000);
+                    
+                }
+                else if (p1 == 0)
+                {
+                    a = 6;
+                }
+                else
+                {
+                    Console.WriteLine("Valor no dentro de 1-2");
+                    Console.WriteLine("Vuelva a intentarlo!");
                 }
             }
-            //Cargar Info
-            else if (p1 == 1)
-            {
-                //try
-                FileStream file = new FileStream("empresas.bin", FileMode.Open);
-                BinaryFormatter formatter = new BinaryFormatter();
-                empresas = (List<Empresa>)formatter.Deserialize(file);
-                //catch
-                //Finally file.close();
-            }
-            //Error
-            else
-            {
-                "Error, valor no dentro del intervalo 0-2, vuelva a intentarlo!"
-            }
+            Console.WriteLine("Gracias por preferirnos!");
+            Console.WriteLine("Vuelva pronto!");
         }
     }
 }
